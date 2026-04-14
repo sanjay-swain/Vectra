b39b3be481e5fa1716dc01a502efc21db0720cbe
Sanjay Swain <sanjayswain450@gmail.com>
2026-04-14 15:57:53 +0530

    Add `SphericalJoint` and its jacobian expression

--- src/system/joints.rs
+++ src/system/joints.rs
@@ -11,9 +11,48 @@ pub struct JacobianRow {
 
 pub trait Joint {
     fn calculate_jacobian(
-        state_a: State,
-        state_b: State,
+        state_a: &State,
+        state_b: &State,
         anchor_a: DVec3,
         anchor_b: DVec3,
     ) -> Vec<JacobianRow>;
+}
+
+pub struct SphericalJoint {}
+
+impl Joint for SphericalJoint {
+    fn calculate_jacobian(
+        state_a: &State,
+        state_b: &State,
+        anchor_a: DVec3,
+        anchor_b: DVec3,
+    ) -> Vec<JacobianRow> {
+        let r_a = state_a.orientation.mul_vec3(anchor_a);
+        let r_b = state_b.orientation.mul_vec3(anchor_b);
+
+        let mut jacobian: Vec<JacobianRow> = vec![];
+
+        jacobian.push(JacobianRow {
+            v_a: DVec3::new(-1.0, 0.0, 0.0),
+            w_a: DVec3::new(0.0, -r_a.z, r_a.y),
+            v_b: DVec3::new(1.0, 0.0, 0.0),
+            w_b: DVec3::new(0.0, r_b.z, -r_b.y),
+        });
+
+        jacobian.push(JacobianRow {
+            v_a: DVec3::new(0.0, -1.0, 0.0),
+            w_a: DVec3::new(r_a.z, 0.0, -r_a.x),
+            v_b: DVec3::new(0.0, 1.0, 0.0),
+            w_b: DVec3::new(-r_b.z, 0.0, r_b.x),
+        });
+
+        jacobian.push(JacobianRow {
+            v_a: DVec3::new(0.0, 0.0, -1.0),
+            w_a: DVec3::new(-r_a.y, r_a.x, 0.0),
+            v_b: DVec3::new(0.0, 0.0, 1.0),
+            w_b: DVec3::new(r_b.y, -r_b.x, 0.0),
+        });
+
+        return jacobian;
+    }
 }
