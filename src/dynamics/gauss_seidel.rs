b347e8b4938f7ca4c9d9047391c08134a7c6ae2b
Sanjay Swain <sanjayswain450@gmail.com>
2026-04-16 22:23:13 +0530

    Refactor gauss seidel implementation and add tests

--- src/dynamics/gauss_seidel.rs
+++ src/dynamics/gauss_seidel.rs
@@ -1,22 +1,66 @@
 pub fn gauss_sediel(a: &Vec<Vec<f64>>, b: &Vec<f64>, x: &mut [f64; 6], iterations: usize) {
-    let mut iter: usize = 0;
     let n = b.len();
 
-    while iter < iterations {
-        let mut i: usize = 0;
-        while i < n {
-            let mut inner_sum: f64 = 0.0;
-            let mut j: usize = 0;
-            while j < n {
-                if j != i {
-                    inner_sum = inner_sum + a[i][j] * x[j];
-                }
-                j = j + 1;
-            }
-            x[i] = (b[i] - inner_sum) / a[i][i];
-            i = i + 1;
-        }
+    for _ in 0..iterations {
+        for i in 0..n {
+            let mut sum = 0.0;
 
-        iter = iter + 1;
+            for j in 0..n {
+                if i != j {
+                    sum += a[i][j] * x[j];
+                }
+            }
+            x[i] = (b[i] - sum) / a[i][i];
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn standard_3_3() {
+        let a = vec![
+            vec![4.0, 1.0, 2.0],
+            vec![1.0, 5.0, 1.0],
+            vec![2.0, 1.0, 6.0],
+        ];
+
+        let b = vec![16.0, 15.0, 24.0];
+        println!("{}", b.len());
+
+        let mut x = [0.0; 6];
+
+        gauss_sediel(&a, &b, &mut x, 200);
+
+        assert!((x[0] - 2.0).abs() < 1e-6);
+        assert!((x[1] - 2.0).abs() < 1e-6);
+        assert!((x[2] - 3.0).abs() < 1e-6);
+    }
+
+    #[test]
+    fn maximum_test() {
+        let a = vec![
+            vec![8.0, 1.0, 2.0, 0.0, 0.0, 0.0],
+            vec![1.0, 10.0, 1.0, 3.0, 0.0, 0.0],
+            vec![2.0, 1.0, 12.0, 1.0, 2.0, 0.0],
+            vec![0.0, 3.0, 1.0, 14.0, 1.0, 1.0],
+            vec![0.0, 0.0, 2.0, 1.0, 16.0, 1.0],
+            vec![0.0, 0.0, 0.0, 1.0, 1.0, 18.0],
+        ];
+
+        let b = vec![16.0, 36.0, 54.0, 76.0, 96.0, 117.0];
+
+        let mut x = [0.0; 6];
+
+        gauss_sediel(&a, &b, &mut x, 30);
+
+        assert!((x[0] - 1.0).abs() < 1e-6);
+        assert!((x[1] - 2.0).abs() < 1e-6);
+        assert!((x[2] - 3.0).abs() < 1e-6);
+        assert!((x[3] - 4.0).abs() < 1e-6);
+        assert!((x[4] - 5.0).abs() < 1e-6);
+        assert!((x[5] - 6.0).abs() < 1e-6);
     }
 }
